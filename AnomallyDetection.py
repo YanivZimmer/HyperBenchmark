59609ab023888bce9b7433045fe9421a2b038e60
YanivZimmer <zimmer.yaniv@gmail.com>
2022-12-17 14:29:27 +0200

    Anomally detection experiment

--- AnomallyDetection.py
+++ AnomallyDetection.py
@@ -1,9 +1,25 @@
 import numpy as np
 from sklearn.covariance import EllipticEnvelope
 heatmap = np.load('data.npy') # load
+heatmap_norm = np.load('data_norm.npy') # load
 print(heatmap)
+print(heatmap_norm)
 # instantiate model
-ee = EllipticEnvelope(contamination = 0.1)#, ensure_min_features=2)#support_fraction=0.7)
+ee = EllipticEnvelope(contamination = 0.033)#, ensure_min_features=2)#support_fraction=0.7)
 # fit model
 results = ee.fit_predict(heatmap)
 print(results)
+ee = EllipticEnvelope(contamination = 0.033)#, ensure_min_features=2)#support_fraction=0.7)
+results_norm = ee.fit_predict(heatmap_norm)
+print(results_norm)
+
+def to_indx(arr):
+    indices = []
+    for i in range(len(arr)):
+        if arr[i] == -1:
+            indices.append(i)
+    return tuple(indices)
+
+
+print(to_indx(results))
+print(to_indx(results_norm))
--- BaseModel.py
+++ BaseModel.py
@@ -26,10 +26,10 @@ NUM_CLASSES = 10
 model = keras.Sequential(
     [
         Dense(2 * INPUT_SHAPE, activation="relu", input_shape=(INPUT_SHAPE,)),
-        Dropout(0.3),
+        Dropout(0.1),
         BatchNormalization(),
         Dense(int(0.5 * INPUT_SHAPE), activation="relu"),
-        Dropout(0.35),
+        Dropout(0.1),
         BatchNormalization(),
         Dense(NUM_CLASSES, activation="sigmoid"),
     ]
@@ -45,37 +45,74 @@ model.compile(
 )
 print(model.summary())
 
+def train_base_model():
+    loader = HyperDataLoader()
+    X, y = loader.generate_vectors("PaviaU")
+    y = to_categorical(y, num_classes=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-loader = HyperDataLoader()
-X, y = loader.generate_vectors("PaviaU")
-y = to_categorical(y, num_classes=10)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    dummy_x = X_train[0].reshape(1, 103)
+    #print("Xtrain pred:", model.predict(dummy_x))
+    v_i = np.zeros((1, 103))
+    v_zeros = np.zeros((1, 103))
+    pred_zero = model.predict(v_zeros)
+    v_i[0][10] = 1
+    pred_i10 = model.predict(v_i)
+    diff_pred = pred_i10 - pred_zero
+    #print("pred_zero", pred_zero, "argmax", np.argmax(pred_zero))
+    #print("pred_i10", pred_i10, "argmax", np.argmax(pred_i10))
+    #print("diff_pred", diff_pred, "argmax", np.argmax(diff_pred))
 
-dummy_x = X_train[0].reshape(1, 103)
-print("Xtrain pred:", model.predict(dummy_x))
-v_i = np.zeros((1, 103))
-v_zeros = np.zeros((1, 103))
-pred_zero = model.predict(v_zeros)
-v_i[0][10] = 1
-pred_i10 = model.predict(v_i)
-diff_pred = pred_i10 - pred_zero
-print("pred_zero", pred_zero, "argmax", np.argmax(pred_zero))
-print("pred_i10", pred_i10, "argmax", np.argmax(pred_i10))
-print("diff_pred", diff_pred, "argmax", np.argmax(diff_pred))
+    history = model.fit(X_train, y_train, epochs=50, batch_size=256, verbose=1)
+    # print(history)
+    results = model.evaluate(X_test, y_test, batch_size=256)
+    print("Accuracy over test set is {0}".format(results))
 
-history = model.fit(X_train, y_train, epochs=40, batch_size=256, verbose=1)
-# print(history)
-results = model.evaluate(X_test, y_test, batch_size=256)
-print("Accuracy over test set is {0}".format(results))
+    pred_zero = model.predict(v_zeros)
+    pred_i10 = model.predict(v_i)
+    diff_pred = pred_i10 - pred_zero
+    #print("pred_zero", pred_zero, "argmax", np.argmax(pred_zero))
+    #print("pred_i10", pred_i10, "argmax", np.argmax(pred_i10))
+    #print("diff_pred", diff_pred, "argmax", np.argmax(diff_pred))
 
-pred_zero = model.predict(v_zeros)
-pred_i10 = model.predict(v_i)
-diff_pred = pred_i10 - pred_zero
-print("pred_zero", pred_zero, "argmax", np.argmax(pred_zero))
-print("pred_i10", pred_i10, "argmax", np.argmax(pred_i10))
-print("diff_pred", diff_pred, "argmax", np.argmax(diff_pred))
+def train_selected_bands_model(bands_mask):
+    input_shape=len(bands_mask)
+    model = keras.Sequential(
+        [
+            Dense(2 * input_shape, activation="relu", input_shape=(input_shape,)),
+            Dropout(0.1),
+            BatchNormalization(),
+            Dense(int(0.5 * input_shape), activation="relu"),
+            Dropout(0.1),
+            BatchNormalization(),
+            Dense(NUM_CLASSES, activation="sigmoid"),
+        ]
+    )
+    LEARNING_RATE = 0.00002
+    opt = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=opt,
+        metrics=[
+            "accuracy",
+        ],
+    )
+    #print(model.summary())
+    loader = HyperDataLoader()
+    X, y = loader.generate_vectors("PaviaU")
+    #print(X.shape)
+    X = X[:, bands_mask]
+    #print(X.shape)
+    y = to_categorical(y, num_classes=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-heatmaps = []
+    #dummy_x = X_train[0].reshape(1, len(X_train[0]))
+    #print("Xtrain pred:", model.predict(dummy_x))
+
+    history = model.fit(X_train, y_train, epochs=50, batch_size=256, verbose=1)
+    print(history)
+    results = model.evaluate(X_test, y_test, batch_size=256)
+    print("Accuracy over test set is {0}".format(results))
 
 
 def build_class_heatmap(model: keras.Model, data, lables, features_num, class_label):
@@ -138,7 +175,30 @@ def feature_contributions(model: keras.Model, features_num, labels_num, normaliz
             arr[i] = arr[i]/np.linalg.norm(arr[i])
     return arr
 
-fc = feature_contributions(model, len(X_test[0]), NUM_CLASSES,True)
-print("fc", fc)
-np.save('data_norm.npy', fc) # save
-print("Done")
+
+if __name__=="__main__":
+    #anomally
+    print("Anomally")
+    #train_selected_bands_model((5, 6, 9, 38, 39, 48, 50, 53, 66, 84, 85))
+    #train_selected_bands_model((5, 6, 48, 50, 66, 84))
+    train_selected_bands_model((5, 48, 50, 84))
+
+    # anomally
+    print("Anomally Norm")
+    #train_selected_bands_model((8, 12, 16, 31, 37, 48, 64, 66, 67, 84, 88))
+    #train_selected_bands_model((16, 31, 37, 48, 67, 88))
+    train_selected_bands_model((16,31,48,88))
+    #random
+    print("Random")
+    random_selected_bands=tuple(np.random.randint(low=1,high=103,size=4))
+    print("Random",random_selected_bands)
+    train_selected_bands_model(random_selected_bands)
+
+    print("All")
+    train_base_model()
+
+
+    #fc = feature_contributions(model, len(X_test[0]), NUM_CLASSES,True)
+    #print("fc", fc)
+    #np.save('data_norm.npy', fc) # save
+    print("Done")
