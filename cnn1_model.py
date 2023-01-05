635ae78d5841fbfcb10494ec54507fade0268fa6
YanivZimmer <zimmer.yaniv@gmail.com>
2023-01-05 18:11:22 +0200

    fix train reshape

--- cnn1_model.py
+++ cnn1_model.py
@@ -41,7 +41,8 @@ def cnn1():
     )
     print(X_train.shape)
     X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
-    #X_train=X_train.reshape(X_train.shape[0],1,X_train.shape[1])
+    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+
     print(X_train.shape)
 
     history = model.fit(X_train, y_train, epochs=50, batch_size=256, verbose=1)
