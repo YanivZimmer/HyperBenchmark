aa4630ecad8b774c5c7079dce26bb6ffd2d63f7b
YanivZimmer <zimmer.yaniv@gmail.com>
2023-02-04 16:11:02 +0200

    Random serach works

--- Experiments/RandomSearch.py
+++ Experiments/RandomSearch.py
@@ -9,10 +9,12 @@ from Experiments.Assesment import Assesment
 from HyperDataLoader.HyperDataLoader import HyperDataLoader
 from models.cnn1_model import cnn_model
 from tensorflow.keras.utils import to_categorical
+import random
 now=datetime.now()
 date_time = now.strftime("%m%d%Y_%H%M")
 logging.basicConfig(filename=f'../logs/Hyperspectral_random_{date_time}.log', filemode='w'
                     ,level=logging.DEBUG,format='%(name)s - %(levelname)s - %(message)s')
+global logger
 logger=logging.getLogger()
 logger.info("")
 
@@ -29,18 +31,19 @@ class RandomSearch(Assesment):
                        X_train,y_train,X_test,y_test)
 
     def random_assess(self,min_bands,attempts_per_bands_amounts,*args,**kwargs):
-        #expect last dim of X_train to be number of bands
+        #expect second last dim of X_train to be number of bands
         best_score = {}
         average_score = {}
         selected = {}
-        amount = self.X_train.shape[-1]
+        amount = self.X_train.shape[-2]
         while amount > min_bands:
             scores = []
             candidates = []
             for i in range(attempts_per_bands_amounts):
-                bands = list(np.random.randint(low=1,high=self.X_train.shape[-1], size=amount))
+                bands = np.sort(np.random.choice(range(1,self.X_train.shape[-2]+1), amount, replace=False))# random.sample(range(1, self.X_train.shape[-2]+1), amount)
                 candidates.append(bands)
-                model, results = self.assess_bands(bands, *args, **kwargs)
+                #bands-1 to norm bands index to match array index and start with 0
+                model, results = self.assess_bands(bands-1, *args, **kwargs)
                 #results[1] is accuracy
                 scores.append(results[1])
             best_score[amount] = max(scores)
@@ -63,7 +66,7 @@ if __name__== '__main__':
     X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
     X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
     searcher = RandomSearch(lambda x: cnn_model(x,NUM_OF_CLASSES), X_train,y_train,X_test,y_test)
-    selected, best_score, average_score = searcher.random_assess(14, 3) #searcher.search_all(list(np.arange(1, 103, 1)),min_bands=14,epochs=100)
-    logger.critical("best_score:=",best_score)
-    logger.critical("average_score:=",average_score)
-    logger.critical("selected:=", selected)
+    selected, best_score, average_score = searcher.random_assess(14, 3,epochs=50) #searcher.search_all(list(np.arange(1, 103, 1)),min_bands=14,epochs=100)
+    logger.info("best_score:=",best_score)
+    logger.info("average_score:=",average_score)
+    logger.info("selected:=", selected)
