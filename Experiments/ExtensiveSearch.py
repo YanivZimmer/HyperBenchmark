556220fa98a7150b2e303a63e08e79ec35d9e8af
YanivZimmer <zimmer.yaniv@gmail.com>
2023-02-22 21:42:10 +0200

    parallel_runs for extensive

--- Experiments/ExtensiveSearch.py
+++ Experiments/ExtensiveSearch.py
@@ -1,19 +1,18 @@
 import logging
 import os
 from datetime import datetime
+from multiprocessing import Pool
 from Experiments.Assesment import Assesment
 now=datetime.now()
 date_time = now.strftime("%m%d%Y_%H%M")
 print(os.getcwd())
-date_time = now.strftime("%m%d%Y_%H%M")
-filename=f'/usr/bin/code/logs/Hyperspectral_extensive_{date_time}.log'
+filename=f'../logs/Hyperspectral_extensive_{date_time}.log'
 logging.basicConfig(level=logging.DEBUG,format='%(name)s - %(levelname)s - %(message)s',handlers=[
         logging.FileHandler(filename),
         logging.StreamHandler()
     ])
 logging.info("hi")
-global logger
-logger=logging.getLogger()
+logger = logging.getLogger()
 logger.info("ya")    
 import statistics
 from typing import List, Callable, Dict
@@ -49,20 +48,48 @@ class ESearcher(Assesment):
             _, [loss,score] = self.assess_bands(almost_all_bands,*args,**kwargs)
             missing_band_to_score[band] = score
         return missing_band_to_score
-    def find_worst(self,results:Dict[int,float])->int:
+    def search_parallel(self, all_bands: List[int],parallel_runs:int,*args,**kwargs) -> Dict[int, float]:
+        missing_band_to_score = {}
+        rem_idx=list(filter(lambda x:x % parallel_runs == 0,range(len(all_bands))))
+        reminder=len(all_bands) % parallel_runs
+        for i in rem_idx[:-2]:
+            scores = self.assess_parallel(all_bands[i:i+parallel_runs],*args,**kwargs)
+            for k in range(parallel_runs):
+                missing_band_to_score[all_bands[i+k]] = scores[k]
+        scores = self.assess_parallel(all_bands[rem_idx[-1]:reminder+rem_idx[-1]],*args,**kwargs)
+        for k in range(reminder):
+            missing_band_to_score[all_bands[rem_idx[-1] + k]] = scores[k]
+        return missing_band_to_score
+
+    def assess_once(self,bands,*args,**kwargs) -> float:
+        _, [loss, score] = self.assess_bands(bands, *args, **kwargs)
+        return score
+    def assess_parallel(self, bands_to_test,all_bands,*args,**kwargs) -> List[float]:
+        almost_all_list = []
+        for band in bands_to_test:
+            almost_all_bands = list(all_bands)
+            almost_all_bands.remove(band)
+            almost_all_list.append(almost_all_bands)
+
+        with Pool() as pool:
+            res = pool.map(self.assess_once, almost_all_list)
+            print(res)
+            return res
+
+    def find_worst(self,results:Dict[int,float]) -> int:
         '''
 
         :return: Return band for which without him the score was highest (this implies he is of less importance )
         '''
 
         return max(results, key=results.get)
-    def search_all(self,all_bands: List[int],min_bands,*args,**kwargs):
+    def search_all(self,all_bands: List[int],min_bands,parallel_runs=2,*args,**kwargs):
         bands = list(all_bands)
         best_score = {}
         average_score = {}
         removed=[]
         while len(bands) > min_bands:
-            results = self.search_without_one(bands,*args,**kwargs)
+            results = self.search_parallel(bands,parallel_runs,*args,**kwargs)
             worst_band=self.find_worst(results)
             removed.append(worst_band)
             score=results[worst_band]
@@ -86,7 +113,7 @@ if __name__== '__main__':
     X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
     X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
     searcher = ESearcher(lambda x: cnn_model(x,NUM_OF_CLASSES),X_train,y_train,X_test,y_test)
-    removed, best_score, average_score = searcher.search_all(list(np.arange(1, 103, 1)),min_bands=14,epochs=75)
+    removed, best_score, average_score = searcher.search_all(list(np.arange(1, 103, 10)),min_bands=14,epochs=75)
     logger.critical("best_score:=",best_score)
     logger.critical("removed:=",removed)
     logger.critical("average_score:=",average_score)
