481f84087bdb5071b1f1af1baf16b332521a67b9
YanivZimmer <zimmer.yaniv@gmail.com>
2023-04-29 17:29:56 +0300

    add mlp with regularization model

---
+++ experiments/pavia_university/regulariztion_mlp.py
@@ -0,0 +1,13 @@
+from models.regularization.regularization_mlp import RegMlpModel
+from models.utils.train_test import train_model, simple_test_model
+from pavia_university import data_loaders, device, NUM_CLASSES_PAVIA,INPUT_SHAPE_PAVIA
+
+
+def main(bands):
+    train_loader, test_loader = data_loaders(bands)
+    mlp = RegMlpModel(INPUT_SHAPE_PAVIA, NUM_CLASSES_PAVIA)
+    train_model(mlp, train_loader, epochs=100, lr=0.000025, device=device,regularization=True)
+    simple_test_model(mlp, test_loader, device=device)
+
+if __name__=='__main__':
+    main(None)
